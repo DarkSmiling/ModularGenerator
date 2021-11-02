ecec5cfeebf5bd1eaf9dcc04a053da2fb6225f1c
bryandrm <bryandrm97@gmail.com>
2021-11-02 15:01:14 -0600

    added cpp class to export a csv file in project location.

---
+++ Source/ModularGenerator/Private/TextFileManager.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TextFileManager.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+
+bool UTextFileManager::SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWritinng = false) 
+{
+	// Set Complete file path.
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+
+	if (!AllowOverWritinng)
+	{
+		if (FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory))
+		{
+			return false;
+		}
+	}
+
+	FString FinalString = "";
+	for (FString& Each : SaveText)
+	{
+		FinalString += Each;
+		FinalString += LINE_TERMINATOR;
+	}
+
+	return FFileHelper::SaveStringToFile(FinalString, *SaveDirectory);
+
+}
+
+
